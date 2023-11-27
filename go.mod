fefd60fe57ff1c2cbc5aa4135fc5a3bec3ece04f
aminbenmansour <amin.benmansour.10@gmail.com>
2023-11-27 07:18:07 +0100

    chore: define region and start an AWS session

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/aminbenmansour/go-serverless
+
+go 1.21.1
+
+require github.com/aws/aws-sdk-go v1.48.4
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+func main() {
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String(region),
+		},
+	)
+
+	if err != nil {
+		return
+	}
+
+}
