c3e6b50f35f73ec0cfb434133bed34da2e54f190
aminbenmansour <amin.benmansour.10@gmail.com>
2023-11-29 03:34:41 +0100

    feat(read): api gateway proxy for fetching users

--- controller/api.go
+++ controller/api.go
@@ -1,13 +1,40 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/aminbenmansour/go-serverless/service"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
 var ErrorMethodNotAllowed = "Method Not Allowed"
 
-func GetUser(req events.APIGatewayProxyRequest, tableName string, dynamoClient dynamodbiface.DynamoDBAPI) {
+type ErrorBody struct {
+	ErrorMsg *string `json:"error,omitempty"`
+}
+
+func GetUser(req events.APIGatewayProxyRequest, tableName string, dynamoClient dynamodbiface.DynamoDBAPI) (
+	*events.APIGatewayProxyResponse, error,
+) {
+	email := req.QueryStringParameters["email"]
+
+	if len(email) > 0 {
+		result, err := service.FetchUser(email, tableName, dynamoClient)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+		return apiResponse(http.StatusOK, result)
+	}
+
+	result, err := service.FetchUsers(tableName, dynamoClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusOK, result)
 
 }
 
