e7e668d9284493f3b1ff16fe078670eeeae3a6db
aminbenmansour <amin.benmansour.10@gmail.com>
2023-12-23 23:56:10 +0100

    feat(apigw|cw): integrate aws clous watch logging at the api gw level

--- infrastructure/apigw.tf
+++ infrastructure/apigw.tf
@@ -8,6 +8,24 @@ resource "aws_apigatewayv2_stage" "lambda" {
 
   name        = "serverless_lambda_stage"
   auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
 }
 
 resource "aws_apigatewayv2_integration" "go_serverless" {
--- infrastructure/cw.tf
+++ infrastructure/cw.tf
@@ -3,3 +3,9 @@ resource "aws_cloudwatch_log_group" "go_serverless" {
 
   retention_in_days = 30
 }
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
