0d64178214d7357a3080269b5f767a6ac45c4e76
aminbenmansour <amin.benmansour.10@gmail.com>
2023-12-23 19:58:01 +0100

    feat(lambda | iam): define an IAM role that allows Lambda to access resources in your AWS account

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -8,4 +8,6 @@ resource "aws_lambda_function" "go_serverless" {
   handler = "main.handler"
 
   source_code_hash = data.archive_file.lambda_go_serverless.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
 }
