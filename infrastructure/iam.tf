9a0e83ff6bd9442f258a78134d0e970d52bcab21
aminbenmansour <amin.benmansour.10@gmail.com>
2023-12-28 01:36:22 +0100

    fix(iac): change undeclared references to actual values

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "lambda_exec" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
+  function_name = aws_lambda_function.go_serverless.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
